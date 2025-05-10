c5daa1902bdbbf7fb5626f5d2dc5fde526c05b38
Ant1och <180115878+Ant1och@users.noreply.github.com>
2025-05-10 16:47:28 +0300

    change wild crops rarities

---
+++ kubejs/data/tfc/worldgen/placed_feature/plant/wild_crop/change_plants_rarity.py
@@ -0,0 +1,21 @@
+from os import listdir
+from os.path import isfile, join
+from math import exp
+
+import json
+import sys
+
+# Every crop will be spawned each 1 in RARITY chunks 
+RARITY = 800
+
+files = [f for f in listdir(".") if isfile(join(".", f))]
+files.remove("change_plants_rarity.py")
+
+for file in files:
+    with open(file, "r") as f:
+        data = json.load(f)
+
+    data["placement"][0]["chance"] = RARITY
+
+    with open(file, "w") as f:
+        json.dump(data, f, indent=2)
